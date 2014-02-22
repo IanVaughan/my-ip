6c82209982fe3f19b6ce847b4192ddca26f6e4c5
Ian Vaughan <github@ianvaughan.co.uk>
2014-02-22 21:17:41 +0000

    Basic hash

--- my-ip.rb
+++ my-ip.rb
@@ -1,14 +1,25 @@
 require 'sinatra'
 require 'json'
 
-ip = 0
+config = {}
 
 get '/' do
-  content_type :json
-  ip
+  "Hi"
 end
 
-get '/update' do
-  ip = request.ip
+get %r{/([a-zA-Z]{6}/update)} do |c|
+  key = c.split('/').first
+  config[key] = request.ip
+end
+
+get %r{/([a-zA-Z]{6}/redirect)} do |c|
+  key = c.split('/').first
+  redirect config[key]
+end
+
+get %r{/([a-zA-Z]{6})} do |c|
+  content_type :json
+  key = c.split('/').first
+  config[key]
 end
 
